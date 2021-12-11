69ce7d631ae811f1653b2198cc59f876f916928d
minji2744 <minji2744@naver.com>
2021-12-11 12:10:02 +0900

    1105 s2 edit

--- coding_test/boj_1105.py
+++ coding_test/boj_1105.py
@@ -1,10 +1,19 @@
-l, r = map(int, input().split())
+l, r = input().split()
 def solution(l,r):
-    count = 1e9
-    for i in range(l,r+1):
-        if '8' not in str(i):
-            return 0
+    count = 0
+    if '8' not in l:
+        return count
+    else:
+        if len(l) < len(r):
+            return count
         else:
-            count = min(count,str(i).count('8'))
-    return count
+            if l == r:
+                return l.count('8')
+            before = True
+            for i in range(len(l)):
+                if before and l[i] == r[i]:
+                    if l[i] == '8':
+                        count += 1
+                else: before = False
+            return count
 print(solution(l,r))
